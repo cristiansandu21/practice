501822de64301c96db9d0c17c39e95ff3f3be011
Cristian Sandu <cristian.sandu@systegra.ro>
2018-05-15 14:57:38 +0300

    Recycler with swipe for delete

---
+++ app/src/main/java/ro/systegra/practice/controllers/CartListAdapter.java
@@ -0,0 +1,82 @@
+package ro.systegra.practice.controllers;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+import ro.systegra.practice.R;
+import ro.systegra.practice.entity.Item;
+
+public class CartListAdapter extends RecyclerView.Adapter<CartListAdapter.MyViewHolder> {
+
+    private Context context;
+    private List<Item> cartList;
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView name, description, price;
+        ImageView thumbnail;
+        RelativeLayout viewBackground, viewForeground;
+
+        public MyViewHolder(View view) {
+            super(view);
+            name = view.findViewById(R.id.name);
+            description = view.findViewById(R.id.description);
+            price = view.findViewById(R.id.price);
+            thumbnail = view.findViewById(R.id.thumbnail);
+            viewBackground = view.findViewById(R.id.view_background);
+            viewForeground = view.findViewById(R.id.view_foreground);
+        }
+    }
+
+    public CartListAdapter(Context context, List<Item> cartList) {
+        this.context = context;
+        this.cartList = cartList;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.card_list_item, parent, false);
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        final Item item = cartList.get(position);
+        holder.name.setText(item.getName());
+        holder.description.setText(item.getDescription());
+        holder.price.setText(item.getPrice() + " Euro");
+
+        Glide.with(context)
+                .load(item.getThumbnail())
+                .into(holder.thumbnail);
+    }
+
+    @Override
+    public int getItemCount() {
+        return cartList.size();
+    }
+
+    public void removeItem(int position) {
+        cartList.remove(position);
+        // notify the item removed by position
+        // to perform recycler view delete animations
+        // NOTE: don't call notifyDataSetChanged()
+        notifyItemRemoved(position);
+    }
+
+    public void restoreItem(Item item, int position) {
+        cartList.add(position, item);
+        // notify item added by position
+        notifyItemInserted(position);
+    }
+}
